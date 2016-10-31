1b840ad9ff1be04b7d54b6da66ad38e0db161dd4
Benjamin Giordano <bengio777@gmail.com>
2016-10-31 15:46:30 -0600

    initial commit again

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,34 @@
+// Libs
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
+import { Meteor } from 'meteor/meteor';
+
+// Modules
+import RoutesConfig from '../routes';
+const App = 'Whatsapp';
+
+// App
+Angular.module(App, [
+  'angular-meteor',
+  'ionic'
+]);
+
+// Startup
+new Loader(App)
+  .load(RoutesConfig);
+ 
+if (Meteor.isCordova) {
+  Angular.element(document).on('deviceready', onReady);
+}
+else {
+  Angular.element(document).ready(onReady);
+}
+
+function onReady() {
+  Angular.bootstrap(document, [App]);
+}
