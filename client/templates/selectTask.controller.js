fe55592cee671147567ca6ee8802dabbc787be5e
Benjamin Giordano <bengio777@gmail.com>
2016-11-01 00:20:41 -0600

    select task feature in progress

---
+++ client/templates/selectTask.controller.js
@@ -0,0 +1,19 @@
+// import { Controller } from 'angular-ecmascript/module-helpers';
+// import { Chats } from '../../../lib/collections';
+//
+// export default class SelectTastCtrl extends Controller {
+//   constructor() {
+//     super(...arguments);
+//
+//     this.taskId = this.$stateParams.taskId;
+//
+//     this.helpers({
+//       data() {
+//         return Tasks.findOne(this.taskId);
+//       }
+//     });
+//   }
+// }
+//
+// SelectTastCtrl.$name = 'SelectTastCtrl';
+// SelectTastCtrl.$inject = ['$stateParams'];
