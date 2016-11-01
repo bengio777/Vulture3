3175806245d2de4eb5a8e168b3dce50507790307
Samuel Persian <samuel.persian@gmail.com>
2016-11-01 15:26:59 -0600

    data changes

--- server/bootstap.js
+++ server/bootstap.js
@@ -1,6 +1,37 @@
 import { Meteor } from 'meteor/meteor';
-// import { Accounts } from 'meteor/accounts-base';
+import { Games, Tasks, Requirements, Images } from '../lib/collections';
 
 Meteor.startup(function() {
-
+  Games.insert([
+      {
+        _id: 0,
+        name: 'Twerk or Treat',
+        // picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+        // timestamp: Moment().subtract(1, 'hours').toDate()
+      },
+      {
+        _id: 1,
+        name: 'Lazer Brigade',
+        // picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
+          // timestamp: Moment().subtract(2, 'hours').toDate()
+      },
+      {
+        _id: 2,
+        name: 'FireFart',
+        // picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
+          // timestamp: Moment().subtract(1, 'days').toDate()
+      },
+      {
+        _id: 3,
+        name: 'Red',
+        // picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
+          // timestamp: Moment().subtract(4, 'days').toDate()
+      },
+      {
+        _id: 4,
+        name: 'Seek and Destroy',
+        // picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
+        // timestamp: Moment().subtract(2, 'weeks').toDate()
+      }
+    ]);
 });
